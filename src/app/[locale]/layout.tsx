a2adc934e266527c4842c72cdc852f8652e58879
Jareer Hassan <jareerhassan8@gmail.com>
2026-03-10 14:51:51 +0500

    df

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,15 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {Locale, routing} from '@/i18n/routing';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { Locale, routing } from '@/i18n/routing';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 
-import './globals.css'
- 
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: {locale: Locale};
-}) {
+import './globals.css';
+
+export default async function LocaleLayout({ children, params }: any) {
   // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
+  const { locale } = await params;
   if (!routing.locales.includes(locale as Locale)) {
     notFound();
   }
