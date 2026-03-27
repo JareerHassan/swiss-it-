109869cdf4c22ef4d4bf91a6e9a3c6fd3fe393bb
Jareer Hassan <jareerhassan8@gmail.com>
2026-03-27 12:13:08 +0500

    hdk

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
  images: {
-  qualities: [75, 85],
   remotePatterns: [
     {
       protocol: 'https',
